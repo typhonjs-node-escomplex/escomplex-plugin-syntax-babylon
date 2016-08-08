bac8fcb59e6902576d2726e4dbb75f4c517575a3
typhonrt <support@typhonrt.org>
2016-08-08 14:09:53 -0700

    moved ast generation usage locally / formatting update

--- src/PluginSyntaxBabylon.js
+++ src/PluginSyntaxBabylon.js
@@ -1,5 +1,6 @@
 import PluginSyntaxESTree  from 'escomplex-plugin-syntax-estree/src/PluginSyntaxESTree';
 
+import ASTParser           from 'typhonjs-escomplex-commons/src/utils/ast/ASTParser';
 import TraitUtil           from 'typhonjs-escomplex-commons/src/module/traits/TraitUtil';
 
 import actualize           from 'typhonjs-escomplex-commons/src/module/traits/actualize';
@@ -32,20 +33,42 @@ export default class PluginSyntaxBabylon extends PluginSyntaxESTree
     */
    ClassMethod()
    {
-      return actualize(0, 0, (node, parent) =>
-       {
-          const operators = TraitUtil.safeComputedOperators(node, parent);
-          operators.push(typeof node.generator === 'boolean' && node.generator ? 'function*' : 'function');
+      return actualize(0, 0, (node) =>
+      {
+         const operators = typeof node.computed === 'boolean' && node.computed ?
+          [...ASTParser.parse(node.key).operators] : [];
 
-          if (node.kind && (node.kind === 'get' || node.kind === 'set')) { operators.push(node.kind); }
-          if (typeof node.async === 'boolean' && node.async) { operators.push('async'); }
-          if (typeof node.static === 'boolean' && node.static) { operators.push('static'); }
-          return operators;
-       },
-       (node, parent) => { return TraitUtil.safeComputedOperands(node, parent); },
-       'key',   // Note: must skip key as the assigned name is determined above.
-       'method'
-      );
+         operators.push(typeof node.generator === 'boolean' && node.generator ? 'function*' : 'function');
+
+         if (node.kind && (node.kind === 'get' || node.kind === 'set')) { operators.push(node.kind); }
+         if (typeof node.async === 'boolean' && node.async) { operators.push('async'); }
+         if (typeof node.static === 'boolean' && node.static) { operators.push('static'); }
+         return operators;
+      },
+      (node, parent) => { return s_SAFE_COMPUTED_OPERANDS(node, parent); },
+      'key',   // Note: must skip key as the assigned name is determined above.
+      (node) =>
+      {
+         let name;
+
+         if (typeof node.computed === 'boolean' && node.computed)
+         {
+            name = node.key.type === 'StringLiteral' ? TraitUtil.safeValue(node.key) :
+             `<computed~${ASTParser.parse(node.key).source}>`;
+         }
+         else // ClassMethod is not computed and is an `Identifier` node.
+         {
+            name = TraitUtil.safeName(node.key);
+         }
+
+         return {
+            type: 'method',
+            name,
+            lineStart: node.loc.start.line,
+            lineEnd: node.loc.end.line,
+            paramCount: node.params.length
+         };
+      });
    }
 
    /**
@@ -69,10 +92,9 @@ export default class PluginSyntaxBabylon extends PluginSyntaxESTree
    DirectiveLiteral()
    {
       return actualize(0, 0, void 0, (node) =>
-       {
-          return typeof node.value === 'string' ? `"${node.value}"` : node.value;
-       }
-      );
+      {
+         return typeof node.value === 'string' ? `"${node.value}"` : node.value;
+      });
    }
 
    /**
@@ -94,12 +116,11 @@ export default class PluginSyntaxBabylon extends PluginSyntaxESTree
    ObjectMethod()
    {
       return actualize(0, 0, (node) =>
-       {
-          return typeof node.kind === 'string' && (node.kind === 'get' || node.kind === 'set') ? node.kind : void 0;
-       },
-       void 0,
-       'key'  // Note: must skip key as the assigned name is forwarded on to FunctionExpression.
-      );
+      {
+         return typeof node.kind === 'string' && (node.kind === 'get' || node.kind === 'set') ? node.kind : void 0;
+      },
+      void 0,
+      'key');  // Note: must skip key as the assigned name is forwarded on to FunctionExpression.
    }
 
    /**
@@ -111,11 +132,10 @@ export default class PluginSyntaxBabylon extends PluginSyntaxESTree
    ObjectProperty()
    {
       return actualize(1, 0, (node) =>
-       {
-          return typeof node.shorthand === 'undefined' ? ':' :
-           typeof node.shorthand === 'boolean' && !node.shorthand ? ':' : void 0;
-       }
-      );
+      {
+         return typeof node.shorthand === 'undefined' ? ':' :
+          typeof node.shorthand === 'boolean' && !node.shorthand ? ':' : void 0;
+      });
    }
 
    /**
@@ -137,4 +157,36 @@ export default class PluginSyntaxBabylon extends PluginSyntaxESTree
     * @returns {{lloc: *, cyclomatic: *, operators: *, operands: *, ignoreKeys: *, newScope: *, dependencies: *}}
     */
    StringLiteral() { return actualize(0, 0, void 0, (node) => { return `"${node.value}"`; }); }
+}
+
+/**
+ * Provides a utility method that determines the operands of a method for Babylon AST nodes. If the name is a computed
+ * value and not a string literal then `ASTGenerator` is invoked to determine the computed operands.
+ *
+ * @param {object}   node - The current AST node.
+ *
+ * @returns {Array<*>}
+ */
+function s_SAFE_COMPUTED_OPERANDS(node)
+{
+   const operands = [];
+
+   if (typeof node.computed === 'boolean' && node.computed)
+   {
+      // The following will pick up a single literal computed value (string).
+      if (node.key.type === 'StringLiteral')
+      {
+         operands.push(TraitUtil.safeValue(node.key));
+      }
+      else // Fully evaluate AST node and children for computed operands.
+      {
+         operands.push(...ASTParser.parse(node.key).operands);
+      }
+   }
+   else // Parent is not computed and `parent.key` is an `Identifier` node.
+   {
+      operands.push(TraitUtil.safeName(node.key));
+   }
+
+   return operands;
 }
